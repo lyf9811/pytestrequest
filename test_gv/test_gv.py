c89e0d1cfbc095f72d2710cc97352bd3442f622a
yinfeng.liu <l98ga11@yeah.net>
2024-07-04 11:04:04 +0300

    7.4接口测试

--- test_gv/test_gv.py
+++ test_gv/test_gv.py
@@ -5,11 +5,13 @@ import requests
 
 # 拼图的newcoming接口
 def test_gv_newcoming():
-        url = "https://puzzle.dev.smartcoder.top/v1/jgspz/home_page/u1/get"
+        # url = "https://puzzle.dev.smartcoder.top/v1/jgspz/home_page/u1/get"
+        url = "https://api.colorplanet.art/v1/jgspz/home_page/u1/get?data=%7B%22app_package_name%22:%22art.color.planet.jigsaw.puzzle.online%22,%22timezone%22:8,%22country%22:%22SG%22,%22platform%22:%22ios%22,%22device_model%22:%22phone%22,%22new_item_version_code%22:%221719991553%22,%22offset%22:0,%22segment_token%22:123,%22muid%22:%22b8a04d2d-1b00-45ec-a2e7-26dce04cb74b%22%7D&skv=K6657942&sig=43c2bae996dbf8296cb65b662918ad27f43c26f0f3e142982139975c444c5e43"
         data = {
-            "data": '{"app_package_name":"art.color.planet.jigsaw.puzzle.online","timezone":8,"country":"CN","platform":"ios","device_model":"phone","new_item_version_code":"1686219710","offset":0,"segment_token":123,"muid":"cda2f41a-6ba0-4006-8d02-50da84151379"}',
-            "skv": "1",
-            "sig": "8cc848329113965c0613d58c6462725fd5896c8acc6ff3d01df9197a3124b085"
+            # "data": '{"app_package_name":"art.color.planet.jigsaw.puzzle.online","timezone":8,"country":"CN","platform":"ios","device_model":"phone","new_item_version_code":"1686219710","offset":0,"segment_token":123,"muid":"cda2f41a-6ba0-4006-8d02-50da84151379"}',
+            "data":'{"app_package_name":"art.color.planet.jigsaw.puzzle.online","timezone":8,"country":"SG","platform":"ios","device_model":"phone","new_item_version_code":"1719991553","offset":0,"segment_token":123,"muid":"b8a04d2d-1b00-45ec-a2e7-26dce04cb74b"}',
+            "skv": "K6657942",
+            "sig": "43c2bae996dbf8296cb65b662918ad27f43c26f0f3e142982139975c444c5e43"
         }
         # res = requests.get(url,params=data,headers=headers)
         res = requests.request("get", url=url, params=data)
